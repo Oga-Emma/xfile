8ad18e4df3e4057f6c519d29b54b5a521d61be5d
Oga-Emma <35017091+Oga-Emma@users.noreply.github.com>
2025-05-01 12:36:34 +0200

    Initial commit

--- src/main/kotlin/app/seven/xfile/MainController.kt
+++ src/main/kotlin/app/seven/xfile/MainController.kt
@@ -3,7 +3,7 @@ package app.seven.xfile
 import javafx.fxml.FXML
 import javafx.scene.control.Label
 
-class HelloController {
+class MainController {
     @FXML
     private lateinit var welcomeText: Label
 
